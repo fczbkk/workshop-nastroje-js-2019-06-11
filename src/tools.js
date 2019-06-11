154cfaee35b662f022bef226d2105622d2bc25e5
Riki Fridrich <riki@fczbkk.com>
2019-06-11 16:51:00 +0200

    feat: add documentation

--- src/tools.js
+++ src/tools.js
@@ -1,12 +1,26 @@
+/**
+ * Adds two numbers.
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}
+ */
 export default function (a, b) {
   return a + b;
 }
 
+/**
+ * Multiplies two numbers
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}
+ */
 export function multiply (a, b) {
   if (typeof a !== 'number' || typeof b !== 'number') {
     throw new Error('multiplier must be number');
   }
   return a * b;
 }
+
+multiply('x', 'y')
 
 export const magic_number = 42;
